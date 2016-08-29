1712c0c82db35a06cf736830ba176d5d157c867c
Wilman Guarin <weguarins@unal.edu.co>
2016-08-28 23:40:50 -0500

    Unificación contactenos, con estilos movilizados

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
-  get 'static_pages/about'
-  get 'static_pages/contact'
-  get 'static_pages_about_path' => 'static_pages/about'
+	get 'static_pages/about'
+	get 'static_pages/contact'
+	get 'static_pages_about_path' => 'static_pages/about'
+	get 'static_pages_contact_path' => 'static_pages/contact'
 
-  get 'welcome/index'
-  root "welcome#index"
-  
-  devise_for :users, controllers: { omniauth_callbacks: 'omniauth_callbacks' }
+	get 'welcome/index'
+	root "welcome#index"
+
+	devise_for :users, controllers: { omniauth_callbacks: 'omniauth_callbacks' }
   match '/users/:id/finish_signup' => 'users#finish_signup', via: [:get, :patch], as: :finish_signup  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
