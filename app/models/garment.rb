b05aef0216107c5398adde2098605509c8bce471
NickZucco <nicastroco@unal.edu.co>
2016-10-01 20:18:15 -0500

    Actualizar modelos Laundry, Garment y Service

--- app/models/garment.rb
+++ app/models/garment.rb
@@ -1,6 +1,6 @@
 class Garment < ApplicationRecord
 	validates :name, presence: true
 	#belongs_to :order
-	has_one :order
-	has_many :services, dependent: :destroy
+	has_many :services
+	has_many :laundries, through: :services
 end
