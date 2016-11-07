9c3a3132ec820f7d7803de883bce5c2bf2962038
ggalvisc <gagalvisc@unal.edu.co>
2016-11-07 16:03:02 -0500

    Draw map and markers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -40,8 +40,23 @@ class UsersController < ApplicationController
 	end
 
 	def orders
-		
 		@laundries = Laundry.all
+		@hash = Gmaps4rails.build_markers(@laundries) do |laundry, marker|
+		  marker.lat laundry.latitude
+		  marker.lng laundry.longitude
+		  content = 	'<div class="content">' +
+						'<div class="image" align="center">' +
+						'<img alt="Laundry Icon" title="Laundry Icon" class="img-responsive" src="/assets/ironing-board.png" />' +
+						'</div>' +
+		  				'<h4 class="name">' + laundry.name + '</h4>' +
+		  				'<span class="address">' + laundry.address + '</span>' + '</br>' +
+		  				'<span class="phone">Phone: ' + laundry.phone.to_s + '</span>' +
+		  				'<h5 class="score">Score: ' + laundry.score.to_s + '</h5>' +
+		  				'<a class="mask" href="/laundries/' + laundry.id.to_s + '">' +
+      						'See prices </a>' +
+						'</div>'
+		  marker.infowindow content
+		end
 	end
 
 	def profile
--- app/models/laundry.rb
+++ app/models/laundry.rb
@@ -1,4 +1,7 @@
 class Laundry < ApplicationRecord
+	geocoded_by :address
+	after_validation :geocode
+	validates :name , :address, presence: true
 	has_many :services, dependent: :destroy
 	has_many :garments, through: :services, dependent: :destroy
 end
