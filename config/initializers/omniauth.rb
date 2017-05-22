1e4f01d4a7a1e6c77226727c937032add2ce9cbb
Andres Gutierrez <agutierrezt@unal.edu.co>
2017-05-21 22:56:08 -0500

    small changes

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 OmniAuth.config.logger = Rails.logger
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, '311877949248617','46ec0a9b2b6dc258828332fa2cafd6cc',:scope => 'user_about_me,user_location, email, user_friends, public_profile' #, :strategy_class => OmniAuth::Strategies::Facebook
+  provider :facebook, '311877949248617','46ec0a9b2b6dc258828332fa2cafd6cc',:scope => 'email, user_friends, public_profile' #, :strategy_class => OmniAuth::Strategies::Facebook
   #provider :twitter, "JpbUFTGUXSpI2mCJpeMybjN1a", "kZW8083GauRwOZAdQ3K5FIyqhzRzhGHaQWuKxxXKntAEnK7l6x"
 end
